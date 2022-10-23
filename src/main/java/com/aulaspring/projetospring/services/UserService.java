f138424cb1307eb00beca4829ae462ebd42889bf
Josivaldo João <josivaldojoao14@gmail.com>
2022-10-22 23:08:54 -0300

    service layer created

---
+++ src/main/java/com/aulaspring/projetospring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.aulaspring.projetospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aulaspring.projetospring.entities.User;
+import com.aulaspring.projetospring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
