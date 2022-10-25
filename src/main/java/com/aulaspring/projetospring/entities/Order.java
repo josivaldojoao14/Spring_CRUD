0d7344822c663e588462f7fa2be4aa9dd0174bc8
Josivaldo João <josivaldojoao14@gmail.com>
2022-10-25 00:22:59 -0300

    subtotal and total methods

--- src/main/java/com/aulaspring/projetospring/entities/Order.java
+++ src/main/java/com/aulaspring/projetospring/entities/Order.java
@@ -98,6 +98,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double sum = 0;
+		for(OrderItem oi : items) {
+			sum += oi.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
